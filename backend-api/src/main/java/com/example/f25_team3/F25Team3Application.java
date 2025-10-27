172daa40267dd1190f417ac0b2bd46db011a615d
aaomarel <aaomarel@uncg.edu>
2025-10-27 18:23:19 -0400

    API backend folder intialized

---
+++ backend-api/src/main/java/com/example/f25_team3/F25Team3Application.java
@@ -0,0 +1,13 @@
+package com.example.f25_team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F25Team3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F25Team3Application.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/com/example/f25_team3/F25Team3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.f25_team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F25Team3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
