e185bf4639f9d1d1084657cee5ac5ed3f14f580c
aaomarel <93951440+aaomarel@users.noreply.github.com>
2025-10-28 11:55:31 -0400

    Delete Backend-api/src/main directory

--- Backend-api/src/main/java/com/example/f25_team3/Costumer/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.csc340.team3.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {}
